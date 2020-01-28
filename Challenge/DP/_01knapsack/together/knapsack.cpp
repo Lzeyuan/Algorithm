4763a9da9e0f32a82f56e59476cc4376c67b968b
Lzeyuan <1046527915@qq.com>
2020-01-28 23:26:37 +0800

    Add Sort Practice

--- Challenge/DP/_01knapsack/together/knapsack.cpp
+++ Challenge/DP/_01knapsack/together/knapsack.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <algorithm>
 #include <cstdio>
+#include <cstring>
 
 using namespace std;
 
-int const MAX_WEIGHT = 100;
+int const MAX_WEIGHT = 450;
 int const MAX_VALUE = 100;
-int const MAX_KNAPSACK = 100;
+int const MAX_KNAPSACK = 700;
 int const MAX_N = 100;
 
 //输入
@@ -16,7 +17,7 @@ int n, weight;
 //01递归
 int rec1(int i, int j){
 	int res;
-	if ( i == n ) {
+	if ( i == n + 1 ) {		//此处是坑，如果输入是从1开始，则dp递归的终点要设置到n+1
 		res = 0;
 	}
 	else if ( j < w[i] ) {
@@ -28,34 +29,45 @@ int rec1(int i, int j){
 	return res;
 }
 
-//02时间优化递归
-int f[MAX_N + 1][MAX_KNAPSACK + 1];
+//02时间优化递归（一开始要初始化数组为-1）
+int dp[MAX_N + 2][MAX_KNAPSACK + 1];		//输入从1开始则MAX_N需要+2，第一行作废
 int rec2(int i, int j) {
+	if( dp[i][j] >= 0 ){
+		return dp[i][j];
+	}
+
 	int res;
-	if (i == n) {
-		res = 0;
+	if ( i == n + 1 ) {		//此处是坑，如果输入是从1开始，则dp递归的终点要设置到n+1
+		res = 0;			//因为，以n为终点，第n个数据没有判断就直接返回0了
+	} else if ( j < w[i] ) {
+		res = rec2( i + 1, j );
+	} else {
+		res = max( rec2(i + 1, j), rec2( i + 1, j - w[i] ) + v[i] );
 	}
-	else if (j < w[i]) {
-		res = rec2(i + 1, j);
-	}
-	else {
-		res = max(rec2(i + 1, j), rec2(i + 1, j - w[i]) + v[i]);
-	}
-	return f[i][j] = res;
+	return dp[i][j] = res;
 }
 
-//03空间+时间优化递归
-int f_space[MAX_N + 1];
+//03循环（正序）
+int dp3[MAX_N + 2][MAX_KNAPSACK + 1];
 int rec3(int j) {
-	for (int i = 1; i <= n; i++) {
-		for (int k = j; k >= w[i] ; k--) {
-			f_space[k] = max( f_space[k], f_space[k - w[i]] + v[i] );
-
-			//查看每次变化
-			for( int m = 0; m <= j; m++ ){
-				cout << f_space[m];
+	for ( int i = 1; i <= n; i++ ) {
+		for ( int k = j; k >= 0; k-- ) {
+			if( k < w[i] ){
+				dp3[i + 1][k] = dp3[i][k];
+			} else {
+				dp3[i + 1][k] = max( dp3[i][k], dp3[i][ k - w[i] ] + v[i] );
 			}
-			printf(" i = %d, k = %d\n", i, k);
+		}
+	}
+	return dp3[n + 1][j];
+}
+
+//04空间+时间优化循环（一定要从尾开始判断）
+int f_space[MAX_N + 1];
+int rec4(int j) {
+	for (int i = 1; i <= n; i++) {
+		for (int k = j; k >= w[i] ; k--) {		//可以将循环判断改成 max( sum(v[i],...,v[n]), w[i] )，但是当物品价值远大于重量就没有意义了
+			f_space[k] = max( f_space[k], f_space[ k - w[i] ] + v[i] );
 		}
 	}
 	return f_space[j];
@@ -68,10 +80,11 @@ int main(){
 		cin >> w[i] >> v[i];
 	}
 	cin >> weight;
-
+	memset( dp, -1, sizeof(dp) );
 	cout << "rec1：" << rec1( 0, weight ) << endl;
-	cout << "rec2：" << rec2( 0, weight ) << endl;
+	cout << "rec2：" << rec2( 1, weight ) << endl;
 	cout << "rec3：" << rec3( weight ) << endl;
+	cout << "rec4：" << rec4( weight ) << endl;
 
 	system("pause");
     return 0;
@@ -79,12 +92,34 @@ int main(){
 
 /*
 测试数据：
+数据一：
 4
 2 3 
 1 2 
 3 4 
 2 2
 5
-输出：
-7
+输出：7
+数据二：
+4
+1 4
+2 6
+3 12
+2 7
+6
+输出：23
+数据三：
+3
+3 1
+2 2
+1 3
+5
+输出：5
+数据四：
+3
+100 2
+300 5
+450 9
+700
+输出：11
 */
---
+++ Relrean/Sort/Bublle.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+using namespace std;
+
+int const MAX = 1E3;
+
+//输入
+int Test[MAX];
+int n;
+
+//冒泡排序
+int BublleSort(int *Target, int count){
+    printf("冒泡排序:\n");
+    int flag = 1;
+    int Swap = 0;
+
+    for( int i = 1; i <= count - 1 && flag; i++){
+        flag = 0;
+        for( int j = 1; j <= count - i; j++ ){
+            if( Target[j] > Target[j + 1] ){
+                flag = 1;
+                Swap = Target[j];
+                Target[j] = Target[j + 1];
+                Target[j + 1] = Swap;
+            }
+        }
+    }
+
+    for( int i = 1; i <= count; i++){
+            printf("%5d", Target[i]);
+    }
+    printf("\n");
+    return 0;
+}
+
+//快速排序
+int QuickSort( int *Target, int Left, int Right){
+    if( Right <= Left )
+        return 0;
+
+    int i = Left;
+    int j = Right;
+    int swap = 0;
+
+    while( i < j ){
+        while( Target[Left] <= Target[j] && i < j )     //检测右边的数，如果遇到比基准数小的停止
+            j--;
+        while( Target[Left] >= Target[i] && i < j )     //检测左边的数，如果遇到比基准数大的停止
+            i++;
+        
+        if( i < j ){        //左边大的和右边小的交换，保证左小右大的顺序
+            swap = Target[i];
+            Target[i] = Target[j];
+            Target[j] = swap;
+        }
+    }
+
+    //第一个数（基准数）放到中间，中间那个数（比基准数小）放到第一位
+    swap = Target[i];
+    Target[i] = Target[Left];
+    Target[Left] = swap;
+
+    //重复上述过程，直到没有数可以拍序
+    QuickSort( Target, Left, i - 1);
+    QuickSort( Target, i + 1, Right);
+
+    return 0;
+}
+
+int main(){
+    //输入
+    cout << "\n============输入==========" << endl;
+    printf("随机生成n个数(0 <= n < 1E3)：");
+    cin >> n;
+    printf("\n");
+
+    cout << "============输出==========" << endl;
+    //生产随机数列
+    srand( (int)time(NULL) );
+    int i;
+    cout << "原数组为：\n";
+    for( i = 1; i <= n; i++ ){
+        Test[i] = rand() % ( n + 1 );
+        printf("%5d", Test[i]);
+        if( i % 10 == 0 )
+            cout << endl;
+    }
+
+    //换行增加美观
+    if( i < 10 )
+        cout << endl;
+
+    //复制原数组，防止原数组被修改
+    int temp[MAX];
+    memcpy( temp, Test, ( n + 1 )* sizeof(int) );
+
+    //冒泡排序
+    BublleSort( temp, n );
+
+    //复制原数组，防止原数组被修改
+    memcpy( temp, Test, ( n + 1 )* sizeof(int) );
+
+    //快速排序
+    printf("快速排序:\n");
+    QuickSort( temp, 1, n );
+    //输出排序结果
+    for( int i = 1; i <= n; i++){
+        printf("%5d", temp[i]);
+    }
+
+    return 0;
+}
